3a30e6570e3d524889191ee77930766e66103938
Ian Cooper <Neuralsilicon@gmail.com>
2021-07-02 11:56:55 -0500

    Initial project setup

---
+++ PieBarChart/PieBarChart.h
@@ -0,0 +1,18 @@
+//
+//  PieBarChart.h
+//  PieBarChart
+//
+//  Created by Ian Cooper on 7/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PieBarChart.
+FOUNDATION_EXPORT double PieBarChartVersionNumber;
+
+//! Project version string for PieBarChart.
+FOUNDATION_EXPORT const unsigned char PieBarChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PieBarChart/PublicHeader.h>
+
+

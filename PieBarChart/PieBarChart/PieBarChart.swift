564ea37299bcdf7693baa7043bcd9e5423dcce7d
Ian Cooper <Neuralsilicon@gmail.com>
2021-07-10 13:42:30 -0500

    fixed problem with Storyboard

--- PieBarChart/PieBarChart/PieBarChart.swift
+++ PieBarChart/PieBarChart/PieBarChart.swift
@@ -38,7 +38,7 @@ public class PieBarChart:UIView{
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     private func initPage(){
